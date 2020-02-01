5099eaf495f85cd958ae1ed0aff92553cecdedf0
erwannbroye <37864396+erwannbroye@users.noreply.github.com>
2020-02-01 09:37:10 +0100

    init push

---
+++ Assets/scripts/IsometricPlayerMovementController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsometricPlayerMovementController : MonoBehaviour
+{
+
+    public float movementSpeed = 1f;
+    IsometricCharacterRenderer isoRenderer;
+
+    Rigidbody2D rbody;
+
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+        isoRenderer = GetComponentInChildren<IsometricCharacterRenderer>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 currentPos = rbody.position;
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector2 inputVector = new Vector2(horizontalInput, verticalInput);
+        inputVector = Vector2.ClampMagnitude(inputVector, 1);
+        Vector2 movement = inputVector * movementSpeed;
+        Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
+        isoRenderer.SetDirection(movement);
+        rbody.MovePosition(newPos);
+    }
+}
---
+++ Assets/scripts/SoundManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+    public class SoundManager : MonoBehaviour 
+    {
+        [HeaderAttribute("File Settings :")]
+        public AudioSource efxSource;
+        public AudioSource musicSource;
+        public static SoundManager instance = null;
+
+
+
+        [HeaderAttribute("Sound Settings :")] 
+        public float minPitchRange = .80f;
+        public float maxPitchRange = 1f;
+
+        void Start ()
+        {
+            if (instance == null)
+                instance = this;
+            else if (instance != this)
+                Destroy (gameObject);
+            DontDestroyOnLoad (gameObject);
+        }
+
+        public void PlayMusic(AudioClip clip)
+        {
+            musicSource.clip = clip;
+            musicSource.Play ();
+        }
+
+        public void PlaySfx(AudioClip clip)
+        {
+            efxSource.clip = clip;
+            efxSource.Play ();
+        }
+
+
+        public void RandomSfx(params AudioClip[] clips)
+        {
+            int index = Random.Range(0, clips.Length);
+            float randomPitch = Random.Range(minPitchRange, maxPitchRange);
+
+            efxSource.pitch = randomPitch;
+            efxSource.clip = clips[index];
+            efxSource.Play();
+        }
+    }
